a3613e7bbd36a3d813e1daef771eeac7c505b121
frank <frank.kessi@eganet.go.tz>
2025-03-23 16:07:01 +0300

    Updated config file with commented SQL queries

--- crud/config.php
+++ crud/config.php
@@ -16,9 +16,43 @@ echo "Database connected successfully !!";
 ?>
 
 
-<!-- INSERT INTO users (first_name,last_name,email,password,gender) VALUES ('Maduka','PCM','madukapcm@gmail.com','@Maduka1','MALE'); -->
+<!-- 
 
-<!--
+  mysql> CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY,fullname VARCHAR(256) NOT NULL,email VARCHAR(256) NOT NULL UNIQUE,password VARCHAR(256) NOT NULL);
+
+  mysql> INSERT INTO employee (fullname,email,password) VALUES('Bro Frank','madukapcm@gmail.com','@maduka1'); 
+
+  mysql> UPDATE employee SET fullname='Maduka Pcm',email='madukapc@gmail.com' WHERE id=1; 
+
+  mysql> DELETE FROM employee WHERE id=1; 
+
+  mysql> CREATE TABLE payment(id INT AUTO_INCREMENT PRIMARY KEY,employee_id INT NOT NULL,amount DECIMAL(10,2) NOT NULL,date DATE NOT NULL,method VARCHAR(50) NOT NULL,FOREIGN KEY (employee_id) REFERENCES employee(id) ON DELETE CASCADE); 
+
+  mysql> INSERT INTO payment (employee_id,amount,date,method) VALUES(2,20000,'2025-03-24','NMB'); 
+
+  mysql> SELECT e.id, e.fullname, e.email, p.amount, p.date, p.method FROM employee e JOIN payment p ON e.id=p.employee_id WHERE p.method='cash' LIMIT 5;
+
+  mysql> SELECT COUNT(*) FROM payment; 
+
+  mysql> SELECT COUNT(*) FROM employee; 
+
+  mysql> SELECT SUM(amount) as total_salary FROM payment;
+
+  mysql> SELECT MAX(amount) AS highest_amount FROM payment;
+
+  mysql> SELECT MIN(amount) AS minimum_salary FROM payment;
+
+  mysql> SELECT AVG(amount) AS average_salary FROM payment;
+
+  mysql> SELECT employee_id, SUM(amount) AS total_paid FROM payment GROUP BY employee_id;
+
+  mysql> SELECT method,COUNT(*) as total_payment FROM payment GROUP BY method;
+
+  mysql> SELECT * FROM employee WHERE fullname LIKE 'maduka';
+
+  mysql> SELECT * FROM employee WHERE fullname LIKE '%maduka%';
+
+
 sudo /opt/lampp/lampp start
 
 sudo /opt/lampp/bin/mysql -u root
@@ -28,5 +62,17 @@ sudo /opt/lampp/lampp restart
 sudo /opt/lampp/lampp status
 
 pass = eG@_2024 port 3306 default.    http://localhost/phpmyadmin
+
+maduka@maduka:~$ mysql -u root -p
+
+mysql> show databases;
+
+mysql> use crud;
+
+mysql> show tables;
+
+mysql> describe table-name;
+
+
 -->
 
